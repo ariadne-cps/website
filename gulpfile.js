ce67790541883a99bdcf56fbd60614154d83eba6
Luca Geretti <luca.geretti@gmail.com>
2018-10-02 18:55:27 +0200

    Add ability to build and FTP the website

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var ftp = require('vinyl-ftp');
+var gutil = require('gulp-util');
+var minimist = require('minimist');
+var args = minimist(process.argv.slice(2));
+gulp.task('deploy', function() {
+  var remotePath = '/www.ariadne-cps.org/';
+  var conn = ftp.create({
+    host: 'ftp.ariadne-cps.org',
+    user: args.user,
+    password: args.password,
+    log: gutil.log
+  });
+  gulp.src(['public/**/*'])
+    .pipe(conn.newer(remotePath))
+    .pipe(conn.dest(remotePath))
+    .pipe(conn.clean('/www.ariadne-cps.org/**/*','public/'));
+});
