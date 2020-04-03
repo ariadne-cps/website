5f702a424c1023bd4f435694e0e5934fa07acdc0
Luca Geretti <luca.geretti@gmail.com>
2020-04-03 14:21:20 +0200

    Fix termination for gulp process

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var ftp = require('vinyl-ftp');
 var gutil = require('gulp-util');
 var minimist = require('minimist');
 var args = minimist(process.argv.slice(2));
-gulp.task('deploy', function() {
+gulp.task('deploy', function(done) {
   var remotePath = '/www.ariadne-cps.org/';
   var conn = ftp.create({
     host: 'ftp.ariadne-cps.org',
@@ -14,4 +14,5 @@ gulp.task('deploy', function() {
   gulp.src(['public/**/*'])
     .pipe(conn.newer(remotePath))
     .pipe(conn.dest(remotePath));
+  done();
 });
